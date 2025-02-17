03bacc0ae431c359f2afda42a8bb26b770909502
ilham b <durianbohong@gmail.com>
2025-02-17 23:10:00 +0700

    migrate ts 1

---
+++ src/controllers/auth/route.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+const router = express.Router();
+
+router.post("/signin", (req: Request, res: Response) => {
+
+})
+
+
+router.post("/signup", (req: Request, res: Response) => {
+    
+})
+
+router.post("/lost-password", (req: Request, res: Response) => {
+    
+})
+
+export default router;
